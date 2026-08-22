81025d4e227fe1702f56e68c4648edff62123a4b
Lean 4 VS Code Extension <>
2026-08-22 00:08:56 -0400

    Initial commit

--- src/type.ml
+++ src/type.ml
@@ -1,4 +1,13 @@
-type arg = string
+type arg = 
+  | APPLY of string (* Apply for rewrite and apply tactics*)
+  | LET of string * string (* apply for variable declaration or calculation tactic*)
+  | UNFOLD of string (* using for unfolding definition and intro variable tactics*)
+  | ALGEBRA (* using for algebraic manipulation tactics such as aesop, grind, omega, congr, etc*)
+  | UNKNOWN of string (* Using for text when say something trivial but not trivial to proof assistant*)
+  | BRANCH of string (* Using branching tactics such as by_cases*)
+  | CALC of string (* Using when performing calculation *)
+  
+
 
 type proof_strategy = 
   DIRECT 
@@ -6,6 +15,8 @@ type proof_strategy =
 | CONTRAPOSITIVE 
 | INDUCTION 
 | UNKNOWN of string
+
+type storage = (string * string * string) list
 
 type proof = proof_strategy * (arg list)
 
