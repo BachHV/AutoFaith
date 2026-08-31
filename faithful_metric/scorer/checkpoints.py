7889c39d62f3573ffedf4b50916ccf54559729db
Lean 4 VS Code Extension <>
2026-08-31 11:13:43 -0400

    Initial commit

--- faithful_metric/scorer/checkpoints.py
+++ faithful_metric/scorer/checkpoints.py
@@ -1,18 +1,23 @@
 
+'''
+This module will define the Checkpoint and Proof Block classes, which will be used to store the state of the proof at various points in time. 
+The Checkpoint class will store the premises and goal of the proof, while the Proof Block class will store the strategy and arguments used to reach that point in the proof.
+'''
+
 class Checkpoint:
     premises : list[str]
     goal : list[str]
 
-class NLchunk:
+class NLblock:
     previous_goals: Checkpoint | None
     arguments : list[str]
 
-class FLchunk:
+class FLblock:
     previous_goals: Checkpoint | None
     arguments : list[str]
 
 
-def create_checkpoint_from_prev_chunks(prev_chunks: list[FLchunk]) -> Checkpoint:
+def create_checkpoint_from_prev_blocks(prev_blocks: list[FLblock]) -> Checkpoint:
     pass
 
 
--- src/Example/Example.lean
+++ src/Example/Example.lean
@@ -1,1 +1,2 @@
-import Example.Basic
+example (p q : Prop) (hp : p) (hq : q) : p ∧ q := by
+  apply And.intro hp hq
